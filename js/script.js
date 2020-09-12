1f3a23d826d00746b7ca9b5e277a369660404225
Liz Keller <sazanamisama@gmail.com>
2020-09-11 21:51:31 -0700

    finish first day tasks

--- js/script.js
+++ js/script.js
@@ -5,5 +5,11 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+$("#shoot").click(function() {
+  const input = $("#input");
+  const choice = input.val();
 
+  $("#userChoice").append(`<div class="userChoice">${choice}</div>`);
 
+  input.val("");
+});
